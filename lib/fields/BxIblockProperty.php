19a910076375fff7a50e25d1b0f49e2d32a34466
postbox433@gmail.com <postbox433@gmail.com>
2016-01-12 15:39:51 +0500

    fix

---
+++ lib/fields/BxIblockProperty.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace serviform\fields;
+
+use \serviform\helpers\Html;
+
+/**
+ * Single checkbox class
+ */
+class BxIblockProperty extends \serviform\FieldBase
+{
+	/**
+	 * @param array
+	 */
+	public $fieldParams = null;
+
+
+	/**
+	 * @return string
+	 */
+	public function getInput()
+	{
+		$res = $this->renderTemplate();
+		if ($res !== null) return $res;
+
+		global $APPLICATION;
+
+		require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/iblock/prolog.php');
+		\Bitrix\Main\Loader::includeModule('iblock');
+		IncludeModuleLangFile($_SERVER['DOCUMENT_ROOT'] . BX_ROOT . '/modules/iblock/admin/iblock_element_edit_compat.php');
+		$APPLICATION->AddHeadScript('/bitrix/js/iblock/iblock_edit.js');
+
+		ob_start();
+		ob_implicit_flush(false);
+		\_ShowPropertyField(
+			$this->getNameChainString(),
+			$this->fieldParams,
+			$this->getValue()
+		);
+		return ob_get_clean();
+	}
+}
