40aeed98d13fda941b9e7adfc763f138497cba9f
postbox433@gmail.com <postbox433@gmail.com>
2016-05-19 11:58:11 +0500

    add tests for textarea field

---
+++ tests/serviform/fields/ButtonTest.php
@@ -0,0 +1,57 @@
+<?php
+
+class ButtonTest extends \tests\cases\Field
+{
+    public function getInputProvider()
+    {
+        return [
+            /*
+            'simple field' => [
+                [
+                    'name' => 'test',
+                ],
+                '<input value="" name="test" type="text">'
+            ],
+            'field with attributes' => [
+                [
+                    'attributes' => [
+                        'type' => 'email',
+                        'class' => 'test',
+                        'data-param' => 1,
+                    ],
+                    'name' => 'test',
+                ],
+                '<input type="email" class="test" data-param="1" value="" name="test">'
+            ],
+            'xss in value' => [
+                [
+                    'name' => 'test',
+                    'value' => '" onclick="alert(\'xss\')" data-param="',
+                ],
+                '<input value="&quot; onclick=&quot;alert(&#039;xss&#039;)&quot; data-param=&quot;" name="test" type="text">'
+            ],
+            'xss in attribute' => [
+                [
+                    'attributes' => [
+                        'type' => '" onclick="alert(\'xss\')" data-param="',
+                    ],
+                    'name' => 'test',
+                ],
+                '<input type="&quot; onclick=&quot;alert(&#039;xss&#039;)&quot; data-param=&quot;" value="" name="test">'
+            ],
+            */
+        ];
+    }
+
+    public function testConfigValue()
+    {
+        $field = $this->getField();
+        $field->config(['value' => 'test']);
+        $this->assertEquals('test', $field->getValue());
+    }
+
+    protected function getField()
+    {
+        return new \serviform\fields\Button;
+    }
+}
