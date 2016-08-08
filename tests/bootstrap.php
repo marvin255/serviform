e69da6dc84aec58cafb5ce176015108a88d39c9d
postbox433@gmail.com <postbox433@gmail.com>
2016-08-08 11:48:41 +0500

    fix tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,8 @@
 $composerAutoloaderPath = dirname(__DIR__) . '/vendor/autoload.php';
 if (file_exists($composerAutoloaderPath)) {
 	require $composerAutoloaderPath;
+} else {
+	require __DIR__ . '/../lib/Autoloader.php';
 }
 
 require __DIR__ . '/cases/Field.php';
