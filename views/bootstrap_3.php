66716b17fb591be0e67f652400ebe6ba59826e66
Your Name <you@example.com>
2017-03-17 00:52:23 +0500

    fix form template for bootstrap 3

--- views/bootstrap_3.php
+++ views/bootstrap_3.php
@@ -1,6 +1,13 @@
 <?php
 
-    use \marvin255\serviform\helpers\Html;
+    use marvin255\serviform\helpers\Html;
+    use marvin255\serviform\interfaces\HasChildren;
+    use marvin255\serviform\fields\Checkbox;
+    use marvin255\serviform\fields\Button;
+    use marvin255\serviform\fields\Input;
+    use marvin255\serviform\fields\Select;
+    use marvin255\serviform\fields\Textarea;
+    use marvin255\serviform\fields\File;
 
     if ($this->getParent()) {
         $tag = 'fieldset';
@@ -10,7 +17,7 @@
 
     echo Html::createTag($tag, $this->getAttributes(), false);
     if ($tag === 'fieldset' && $this->getLabel()) {
-        echo Html::createTag('legend', null, $this->getLabel());
+        echo Html::createTag('legend', null, Html::clearAttributeValue($this->getLabel()));
     }
 
     $els = $this->getElements();
@@ -26,43 +33,41 @@
     <?php foreach ($els as $el):
         $errors = $el->getErrors();
     ?>
-        <?php if ($el instanceof \marvin255\serviform\interfaces\HasChildren): ?>
-            <div class="form-group">
-                <?php if ($el->getLabel()): ?>
-                    <label>
-                        <?php echo Html::clearAttributeValue($el->getLabel()); ?>
-                    </label>
-                <?php endif; ?>
-                <?php echo $el->getInput(); ?>
-            </div>
-        <?php elseif ($el instanceof \marvin255\serviform\fields\Checkbox): ?>
+        <?php if ($el instanceof Checkbox): ?>
             <div class="checkbox<?php if (!empty($errors)) echo ' has-error'; ?>">
                 <?php echo $el->getInput(); ?>
                 <label for="<?php echo Html::clearAttributeValue($el->getAttribute('id')); ?>">
                     <?php echo Html::clearAttributeValue($el->getLabel()); ?>
                 </label>
                 <?php if (!empty($errors)): ?>
-                    <span class="help-block"><?php echo implode(', ', $errors); ?></span>
+                    <span class="help-block">
+                        <?php echo Html::clearAttributeValue(implode(', ', $errors)); ?>
+                    </span>
                 <?php endif; ?>
             </div>
-        <?php elseif ($el instanceof \marvin255\serviform\fields\Button): ?>
-            <?php echo $el->addToAttribute('class', ' btn')->getInput(); ?>
-        <?php elseif ($el instanceof \marvin255\serviform\fields\HtmlText): ?>
+        <?php elseif ($el instanceof HasChildren): ?>
             <?php echo $el->getInput(); ?>
+        <?php elseif ($el instanceof Button): ?>
+            <?php echo $el->addToAttribute('class', ' btn')->getInput(); ?>
         <?php else: ?>
             <div class="form-group<?php if (!empty($errors)) echo ' has-error'; ?>">
-                <?php if (!$this->getParent() || $el->getLabel()): ?>
+                <?php if ($el->getLabel()): ?>
                     <label for="<?php echo Html::clearAttributeValue($el->getAttribute('id')); ?>">
                         <?php echo Html::clearAttributeValue($el->getLabel()); ?>
                     </label>
                 <?php endif; ?>
                 <?php
-                    echo $el->addToAttribute('class', ' form-control')->getInput();
+                    if ($el instanceof Input || $el instanceof Select || $el instanceof Textarea) {
+                        $el->addToAttribute('class', ' form-control');
+                    }
+                    echo $el->getInput();
                 ?>
                 <?php if (!empty($errors)): ?>
-                    <span class="help-block"><?php echo implode(', ', $errors); ?></span>
+                    <span class="help-block">
+                        <?php echo Html::clearAttributeValue(implode(', ', $errors)); ?>
+                    </span>
                 <?php endif; ?>
-            </div>
+            </div>         
         <?php endif; ?>
     <?php endforeach; ?>
 <?php echo Html::createCloseTag($tag); ?>
