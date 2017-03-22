f5e558c503b498a2c45961ab5e6582d96c292fd0
marvin255 <postbox433@gmail.com>
2017-03-22 15:03:38 +0500

    Update File.php

--- src/fields/File.php
+++ src/fields/File.php
@@ -2,19 +2,25 @@
 
 namespace marvin255\serviform\fields;
 
+use marvin255\serviform\helpers\Html;
+use marvin255\serviform\abstracts\Field;
+
 /**
  * File class.
  */
-class File extends Input
+class File extends Field
 {
     /**
      * @return string
      */
-    public function getInput()
+    protected function renderInternal()
     {
-        $this->setAttribute('type', 'file');
+        $attrubutes = $this->getAttributes();
+        $attrubutes['type'] = 'file';
+        $attrubutes['value'] = $this->getValue();
+        $attrubutes['name'] = $this->getNameChainString();
 
-        return parent::getInput();
+        return Html::createTag('input', $attrubutes, false);
     }
 
     /**
